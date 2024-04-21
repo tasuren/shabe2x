d724d08175f1adac146c82da85d06f11f4557e00
Takagi Tasuku <tasuren@outlook.jp>
2024-04-21 15:33:14 +0900

    ホーム画面のレイアウトの調整

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <div class="flex" style:height="calc(100vh - 5rem)">
 	<div class="my-auto">
-		<h1 class="m-0 text-2xl sm:text-5xl">快適なシャドーイングアプリ</h1>
+		<h1 class="m-0 break-keep sm:text-5xl">快適な<wbr />シャドーイングアプリ</h1>
 		<p>HTMLのコピーペーストや部分選択による再生にも対応</p>
 	</div>
 </div>
