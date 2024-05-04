4507f77ddc72225667a8c997f1de831cacef272c
Takagi Tasuku <tasuren@outlook.jp>
2024-05-04 18:50:54 +0900

    fix ツールチップが右端にくっつくと縦に長くなり、選択箇所を隠す #4

--- src/lib/components/app/Tooltip.svelte
+++ src/lib/components/app/Tooltip.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 	import type { Speaker } from "./speaker";
 	import { stringifyNode } from "./stringify";
 
 	export let speaker: Speaker<any>;
-	export let hidden = true,
+	export let hidden = false,
 		x = 0,
 		y = 0;
-	export let element: HTMLDivElement | null = null;
 	let outerPlay: (rate: number, node: Node) => void;
 	export { outerPlay as play };
 
+	let element: HTMLDivElement;
 	let selectedContents: DocumentFragment | null = null;
 	let play = (rate: number) => {
 		if (selectedContents) outerPlay(rate, selectedContents);
@@ -27,7 +28,15 @@
 		return `https://translate.google.com/?${params.toString()}`;
 	}
 
+	// ツールチップの横幅を得る。
+	let tooltipWidth: number = 0;
+	onMount(() => {
+		tooltipWidth = element.getBoundingClientRect().width;
+		hidden = true;
+	});
+
 	addEventListener("mouseup", (event) => {
+		// 選択した際にツールチップを表示して動かしたりする。
 		if (isMouseHover) return;
 
 		const selection = getSelection();
@@ -35,7 +44,11 @@
 		const range = selection.getRangeAt(0);
 
 		if (!range.collapsed) {
+			// 選択箇所のすぐそこにツールチップを移動させる。
 			x = event.clientX + scrollX;
+			if (x + tooltipWidth > innerWidth)
+				// ツールチップが右端に突き刺さらないように考慮する。
+				x = innerWidth - tooltipWidth;
 
 			// Y座標はマウスカーソルの位置に近い方の選択箇所の向こう側とする。
 			const rect = range.getBoundingClientRect();
@@ -47,9 +60,8 @@
 			else y = event.clientY + scrollY + 15;
 
 			hidden = false;
+			selectedContents = range.cloneContents();
 		}
-
-		selectedContents = range.cloneContents();
 	});
 
 	let isMouseHover = false;
@@ -59,37 +71,37 @@
 </script>
 
 {#key [x, y]}
-	{#if !hidden}
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<!-- svelte-ignore a11y-mouse-events-have-key-events -->
-		<div
-			id="app-tooltip"
-			class="
-        // レイアウト
-        absolute flex space-x-2 p-2
-        // 見た目
-        backdrop-blur-md rounded-md
-      "
-			style:left="{x}px"
-			style:top="{y}px"
-			on:mouseover={() => (isMouseHover = true)}
-			on:mouseleave={() => (isMouseHover = false)}
-			bind:this={element}
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+	<div
+		id="app-tooltip"
+		class="
+			// レイアウト
+			absolute space-x-2 p-2
+			// 見た目
+			backdrop-blur-md rounded-md
+		"
+		class:hidden
+		class:flex={!hidden}
+		style:left="{x}px"
+		style:top="{y}px"
+		on:mouseover={() => (isMouseHover = true)}
+		on:mouseleave={() => (isMouseHover = false)}
+		bind:this={element}
+	>
+		<button
+			type="button"
+			on:click={() => {
+				play(0.5);
+			}}>部分ゆっくり再生</button
 		>
-			<button
-				type="button"
-				on:click={() => {
-					play(0.5);
-				}}>部分ゆっくり再生</button
-			>
-			<button
-				type="button"
-				on:click={() => {
-					play(1);
-				}}>部分再生</button
-			>
-			<button type="button" on:click={speaker.stop}>停止</button>
-			<a href={makeGoogleTranslateURL()} target="_blank">Google 翻訳で開く</a>
-		</div>
-	{/if}
+		<button
+			type="button"
+			on:click={() => {
+				play(1);
+			}}>部分再生</button
+		>
+		<button type="button" on:click={speaker.stop}>停止</button>
+		<a href={makeGoogleTranslateURL()} target="_blank">Google 翻訳で開く</a>
+	</div>
 {/key}
