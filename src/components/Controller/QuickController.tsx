f29f9c0eebb36770230624093c9f6dfab2832d48
Takagi Tasuku <tasuren@outlook.jp>
2025-01-09 22:18:42 +0900

    QuickControllerが外側をクリックした際に消えるよう実装

--- src/components/Controller/QuickController.tsx
+++ src/components/Controller/QuickController.tsx
@@ -1,4 +1,4 @@
-import { Mouse } from "@/lib/mouse-collector";
+import { Mouse, type PagePosition } from "@/lib/mouse-collector";
 import { stringifyNode } from "@/lib/paper";
 import { Show, createSignal, onCleanup, onMount } from "solid-js";
 import VoiceController from "./VoiceController";
@@ -11,6 +11,33 @@ function makeGoogleTranslateURL(document: DocumentFragment): string {
     });
 
     return `https://translate.google.com/?${params.toString()}`;
+}
+
+/** QuickControllerを表示すべき場所を、選択箇所とマウスカーソルの場所を元に算出する。 */
+async function calculatePosition(
+    mouse: Mouse,
+    range: Range,
+    elementWidth: number,
+): Promise<PagePosition | null> {
+    let [x, y] = [0, 0];
+    const mousePosition = await mouse.getMousePosition();
+
+    // 選択箇所のすぐそこにツールチップを移動させる。
+    x = mousePosition.x + scrollX;
+
+    // ツールチップが右端に突き刺さらないように考慮する。
+    if (x + elementWidth > innerWidth) x = innerWidth - elementWidth;
+
+    // Y座標はマウスカーソルの位置に近い方の選択箇所の向こう側とする。
+    const rect = range.getBoundingClientRect();
+    if (mousePosition.y < rect.y + rect.height / 2)
+        // もしも選択箇所の半分より上にマウスカーソルがあるなら、
+        // 選択箇所の上側の向こう側をY座標とする。
+        y = rect.top + scrollY - 50;
+    // 選択箇所の下側のY座標。
+    else y = mousePosition.y + scrollY + 15;
+
+    return { x, y };
 }
 
 function QuickController() {
@@ -27,41 +54,36 @@ function QuickController() {
     const onSelectionChange = async () => {
         // 選択箇所を取得する。
         const selection = getSelection();
-        if (!selection || selection.rangeCount === 0) return;
+        if (!selection || selection.rangeCount === 0) return null;
         const range = selection.getRangeAt(0);
 
         // 選択箇所が幅０の場合、何も選択していないとみなす。
-        if (range.collapsed) return;
+        if (range.collapsed) return null;
 
-        let [x, y] = [0, 0];
-        const mousePosition = await mouse.getMousePosition();
+        // QuickControllerを配置すべき場所を算出する。
+        const position = await calculatePosition(mouse, range, elementWidth);
 
-        // 選択箇所のすぐそこにツールチップを移動させる。
-        x = mousePosition.x + scrollX;
+        if (position) {
+            setPosition(position);
+            setHidden(false);
+            setSelectedContents(range.cloneContents());
+        }
+    };
 
-        // ツールチップが右端に突き刺さらないように考慮する。
-        if (x + elementWidth > innerWidth) x = innerWidth - elementWidth;
+    const [isUsing, setUsing] = createSignal(false);
 
-        // Y座標はマウスカーソルの位置に近い方の選択箇所の向こう側とする。
-        const rect = range.getBoundingClientRect();
-        if (mousePosition.y < rect.y + rect.height / 2)
-            // もしも選択箇所の半分より上にマウスカーソルがあるなら、
-            // 選択箇所の上側の向こう側をY座標とする。
-            y = rect.top + scrollY - 50;
-        // 選択箇所の下側のY座標。
-        else y = mousePosition.y + scrollY + 15;
-
-        setPosition({ x, y });
-        setHidden(false);
-        setSelectedContents(range.cloneContents());
+    const onMouseDown = () => {
+        if (!isUsing()) setHidden(true);
     };
 
     onMount(() => {
         document.addEventListener("selectionchange", onSelectionChange);
+        addEventListener("mousedown", onMouseDown);
     });
 
     onCleanup(() => {
         document.removeEventListener("selectionchange", onSelectionChange);
+        removeEventListener("mousedown", onMouseDown);
         mouse.close();
     });
 
@@ -70,6 +92,9 @@ function QuickController() {
             class="absolute space-x-2 p-2 z-50"
             classList={{ hidden: hidden() }}
             style={{ left: `${position().x}px`, top: `${position().y}px` }}
+            // biome-ignore lint: マウスを使う前提のボタン群なので、onFocusは↓とセットで使わない。
+            onMouseOver={() => setUsing(true)}
+            onMouseLeave={() => setUsing(false)}
             ref={(element) => {
                 elementWidth = element.getBoundingClientRect().width;
                 setHidden(true);
